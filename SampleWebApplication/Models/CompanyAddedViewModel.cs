02c02865b81d02623987b67b5965382b96c3a312
arturowicz <artur.teo@wp.pl>
2020-10-11 17:23:55 +0200

    Add company controller

---
+++ SampleWebApplication/Models/CompanyAddedViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleWebApplication.Models
+{
+    public class CompanyAddedViewModel
+    {
+        public int NumberOfCharsInName { get; set; }
+        public int NumberOfCharsInDescription { get; set; }
+        public bool IsHidden { get; set; }
+    }
+}
---
+++ SampleWebApplication/Models/CompanyModel.cs
@@ -0,0 +1,9 @@
+namespace SampleWebApplication.Models
+{
+    public class CompanyModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
